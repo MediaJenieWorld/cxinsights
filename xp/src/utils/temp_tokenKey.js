eddeda150f8d599e565d84246869215fefb068b6
rutvikSurani <rutviksurani0123@gmail.com>
2024-07-11 17:08:18 +0530

    fix:minor changed

--- xp/src/utils/temp_tokenKey.js
+++ xp/src/utils/temp_tokenKey.js
@@ -1,3 +1,6 @@
 export const encodeKey = "Xperiento-db";
-export const cookiesKey = "Xperiento-cookies";
+export const cookiesKey = "Xperiento-cookies"; 
+
 export const server_base_Url = "http://localhost:5055/";
+
+// export const server_base_Url = "https://xperiento.mediajenie.com/api/"
